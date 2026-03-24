1df470d57f16d7c1cf61dd713492fbb3ceeae487
mdemirbas <mmdemirbas@gmail.com>
2026-03-24 20:32:16 +0300

    Add Kover plugin to enable test coverage reporting

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.compose)
     alias(libs.plugins.compose.compiler)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kover)
 }
 
 group = "com.github.mmdemirbas.icelens"
